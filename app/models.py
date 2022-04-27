4e05b90031602be5f12db04a5dcac54ce57b6253
EmmaK <emmaculatewkamau@gmail.com>
2022-04-27 16:17:32 +0300

    Add test methods

--- app/models.py
+++ app/models.py
@@ -3,6 +3,9 @@ Define and initialize classes for our objects
 '''
 
 class Books:
+
+    books_list = []
+
     def __init__(self,id,title,authors,publishedDate,description,language):
         self.id = id
         self.title = title
@@ -10,3 +13,12 @@ class Books:
         self.publishedDate=publishedDate
         self.description = description
         self.language = language
+
+    def save_book(self):
+        Books.books_list.append(self)
+
+    @classmethod
+    def display_books(cls):
+        return cls.books_list
+
+    pass
--- tests/test_books.py
+++ tests/test_books.py
@@ -1,13 +1,18 @@
 import unittest
-from ..app.models import Books
+from app.models import Books
 
-#id,title,authors,publishedDate,description,language
+'''
+BDD:
+    1. Save an object
+    2. Save multiple objects
+    3. Display all objects
+'''
 class BookTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.new_book = Books(1,"The 5AM Club","Robin Sharma","20/09/1990","Lorem Ipsum","Eng")
+        self.new_book = Books(1,"The 5AM Club","Robin Sharma","20/09/1990","Lorem Ipsum","eng")
 
     def tearDown(self) -> None:
-        self.book_list = []
+        Books.books_list = []
 
     def test_init(self):
         self.assertEqual(self.new_book.id,1)
@@ -15,7 +20,20 @@ class BookTest(unittest.TestCase):
         self.assertEqual(self.new_book.authors,"Robin Sharma")
         self.assertEqual(self.new_book.publishedDate,"20/09/1990")
         self.assertEqual(self.new_book.description,"Lorem Ipsum")
-        self.assertEqual(self.new_book.language,"Eng")
+        self.assertEqual(self.new_book.language,"eng")
+
+    def test_save_book(self):
+        self.new_book.save_book()
+        self.assertEqual(len(Books.books_list),1)
+
+    def test_multiple_books(self):
+        self.new_book.save_book()
+        test_book = Books(2,"Gifted Hands","Ben Carson","23/12/1994","Lorem Ipsum","eng")
+        test_book.save_book()
+        self.assertEqual(len(Books.books_list),2)
+
+    def test_display_all_books(self):
+        self.assertEqual(Books.display_books(), Books.books_list)
 
 if __name__ == '__main__':
     unittest.main()
